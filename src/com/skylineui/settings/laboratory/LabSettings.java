9b35428ee9dd9d4f7291ef2d6142fa2a1259a2d7
aswin7469 <aswinas@pixysos.com>
2023-01-13 20:25:48 +0530

    Settings: Add Toggle for GamesProp
    Change-Id: Id2c0f8f4674ecfe75dede1a2bfee7736f8273ac5
    
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Co-Authored-By: Pranav Vashi <neobuddy89@gmail.com>
    Co-Authored-By: spezi77 <spezi7713@gmx.net>

--- src/com/skylineui/settings/laboratory/LabSettings.java
+++ src/com/skylineui/settings/laboratory/LabSettings.java
@@ -25,6 +25,7 @@ import android.content.DialogInterface;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.UserHandle;
+import android.os.SystemProperties;
 import android.provider.SearchIndexableResource;
 import android.provider.Settings;
 import android.text.TextUtils;
@@ -50,18 +51,39 @@ import java.util.List;
 public class LabSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener, Indexable {
 
+    private static final String KEY_GAMES_SPOOF = "use_games_spoof";
+
+    private static final String SYS_GAMES_SPOOF = "persist.sys.pixelprops.games";
+
+    private SwitchPreference mGamesSpoof;
+
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         ContentResolver resolver = getActivity().getContentResolver();
         addPreferencesFromResource(R.xml.skylineui_lab_settings);
+        final PreferenceScreen prefScreen = getPreferenceScreen();
 
-        final Resources res = getResources();
+        mGamesSpoof = (SwitchPreference) prefScreen.findPreference(KEY_GAMES_SPOOF);
+        mGamesSpoof.setChecked(SystemProperties.getBoolean(SYS_GAMES_SPOOF, false));
+        mGamesSpoof.setOnPreferenceChangeListener(this);
+
+    }
+
+    public static void reset(Context mContext) {
+        ContentResolver resolver = mContext.getContentResolver();
+        SystemProperties.set(SYS_GAMES_SPOOF, "false");
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mGamesSpoof) {
+            boolean value = (Boolean) newValue;
+            SystemProperties.set(SYS_GAMES_SPOOF, value ? "true" : "false");
+            return true;
+
+        }
         return false;
     }
 
